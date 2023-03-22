98526d33f39c2aa3ed5c6efc60b01fba728763f0
danielzk107 <danielzk107@gmail.com>
2023-03-22 13:47:51 +0200

    Finished all functions

--- AdptArray.c
+++ AdptArray.c
@@ -19,16 +19,20 @@ typedef struct PAdptArray
 
 PAdptArray CreateAdptArray(COPY_FUNC copy_func, DEL_FUNC del_func, PRINT_FUNC print_func)
 {
-   struct PAdptArray parr;
-   parr.copy_func = copy_func;
-   parr.length = 0;
-   parr.size = 20;
-   parr.indexarr = malloc(sizeof(int));
-   parr.arr = malloc(20 * sizeof(PElement)); // Starting size of 20
-   // if(sizeof(parr.arr) != 20*sizeof(PElement)){
-   //    return  NULL;
-   // }
-   return parr;
+   struct PAdptArray *parr = malloc(sizeof(PAdptArray));
+   parr -> print_func = print_func;
+   parr -> del_func = del_func;
+   parr->copy_func = copy_func;
+   parr->length = 0;
+   parr->size = 20;
+   parr->indexarr = malloc(sizeof(int));
+   parr->arr = malloc(20 * sizeof(PElement)); // Starting size of 20
+   if(sizeof(parr->arr) != 20*sizeof(PElement)){
+      parr->length = -1;
+      parr->size = -1;
+      // return NULL;
+   }
+   return *parr;
 }
 void DeleteAdptArray(PAdptArray parr)
 {
@@ -57,20 +61,45 @@ int Index_In_Parr(PAdptArray parr, int index){ // Function used to check if a ce
 
 Result SetAdptArrayAt(PAdptArray parr, int index, PElement pel){
    int index_in_parr = Index_In_Parr(parr, index);
-   PElement new_pel = &pel;
+   PElement new_pel = parr.copy_func(pel);
    if(index_in_parr == -1){ // Index is not used in parr
-      parr.length++;
-      parr.indexarr = realloc(parr.indexarr, parr.length * sizeof(int));
-      parr.indexarr[parr.length] = index;
-      if(index > parr.size){
+      if(index > parr.size){ // Index given is larger than the size of arr
          parr.size = index;
          parr.arr = realloc(parr.arr, index * sizeof(PElement));
+         if(parr.arr == NULL){
+            return FAIL;
+         }
       }
       parr.arr[index] = new_pel;
+      parr.length++;
+      parr.indexarr = realloc(parr.indexarr, parr.length * sizeof(int));
+      if(parr.indexarr == NULL){
+         return FAIL;
+      }
+      parr.indexarr[parr.length] = index;
    }
    else{ // Index is used in parr
       parr.indexarr[index_in_parr] = index;
-      parr.del_func(parr.arr[index_in_parr]);
+      parr.del_func(parr.arr[index]);
       parr.arr[index] = new_pel;
    }
+   return SUCCESS;
 }
+
+PElement GetAdptArrayAt(PAdptArray parr, int index){
+   if(Index_In_Parr(parr, index) == -1){
+      return NULL;
+   }
+   return parr.copy_func(parr.arr[index]);
+}
+
+int GetAdptArraySize(PAdptArray parr){
+   return parr.length;
+}
+
+void PrintDB(PAdptArray parr){
+   for(int i = 0; i< parr.length; i++){
+      parr.print_func(parr.arr[parr.indexarr[i]]);
+   }
+}
+
